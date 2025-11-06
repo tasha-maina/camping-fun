5f82b5987f831634663993516ab21553c4bbad72
tasha-maina <tashabanks271@gmail.com>
2025-11-06 21:32:00 +0300

    Update database URI to use environment variable

--- server/config.py
+++ server/config.py
@@ -7,7 +7,7 @@ migrate = Migrate()
 
 def create_app():
     app = Flask(__name__)
-    app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///camping.db"
+    app.config["SQLALCHEMY_DATABASE_URI"] = "os.getenv("DATABASE_URL")
     app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
 
     db.init_app(app)
